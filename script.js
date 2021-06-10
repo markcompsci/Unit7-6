8140981ba387f2026fb7d80a9c60f493899ec798
markcompsci <84529187+markcompsci@users.noreply.github.com>
2021-06-10 14:32:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const MY_AGE = 44
+let guessNum = 0
+let userGuess
+let lowhi
+
+while (userGuess !== MY_AGE) {
+  if (userGuess > MY_AGE) {
+    lowhi = "lower"
+  } else {
+    lowhi = "higher"
+  }
+  console.log(lowhi)
+  if (guessNum !== 0) {
+  userGuess = parseInt(prompt(`Try Again!\nWhat is my age?\nHint: My age is ${lowhi} than your guess.`))
+  } else {
+    userGuess = parseInt(prompt("What is my age?"))
+  })
+  guessNum++
+  cosole.log(userGuess))
+}
